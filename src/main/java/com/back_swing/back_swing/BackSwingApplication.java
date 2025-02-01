66c7678119874b5b495b295a5465b1ea382fa7f2
Miguel Castro Franco <miguelangelcf52@gmail.com>
2025-01-31 23:34:35 -0500

    First Commit
    First Commit

---
+++ src/main/java/com/back_swing/back_swing/BackSwingApplication.java
@@ -0,0 +1,13 @@
+package com.back_swing.back_swing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSwingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackSwingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/back_swing/back_swing/BackSwingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back_swing.back_swing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSwingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
